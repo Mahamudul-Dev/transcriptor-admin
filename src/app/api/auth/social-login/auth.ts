3f056b62fbcac8ba5978912c9de68c5331c465c0
Mahamudul.Dev <mahamudul.dev@gmail.com>
2025-06-12 01:49:53 +0600

    Update auth.ts

--- src/app/api/auth/social-login/auth.ts
+++ src/app/api/auth/social-login/auth.ts
@@ -38,12 +38,15 @@ export function getAppleSigningKey(
 export function getFirebaseAdminApp() {
   if (app) return app;
 
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n");
+  console.log(privateKey);
+
   if (!admin.apps.length) {
     app = admin.initializeApp({
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        privateKey: privateKey,
       }),
     });
   }
